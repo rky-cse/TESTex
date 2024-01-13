ddcf96e2c5506043a79a568f8ba69dde5bd3c6a9
Ravi Kumar Yadav <ravi27777777@gmail.com>
2024-01-13 17:35:34 +0530

    bug fix

--- frontend/src/TeacherHome.js
+++ frontend/src/TeacherHome.js
@@ -1,11 +1,7 @@
-<<<<<<< HEAD
-import React, { useState } from 'react';
-import { Link } from 'react-router-dom';
-=======
 
 import React, { useState, useEffect } from 'react';
+import {Link} from "react-router-dom"
 import SingleCorrectQuestionForm from './SingleCorrectQuestionForm';
->>>>>>> cf212c931528e3fffafe32a42aee6f5958f00ba7
 
 const TeacherHome = ({ userInfo }) => {
   const [creatingMockTest, setCreatingMockTest] = useState(false);
@@ -33,52 +29,8 @@ const TeacherHome = ({ userInfo }) => {
 
       <button onClick={handleLogout}>Logout</button>
 
-<<<<<<< HEAD
+
       
-=======
-      {creatingMockTest && (
-        <div>
-          <label>
-            Test Name:
-            <input
-              type="text"
-              value={mockTestName}
-              onChange={(e) => setMockTestName(e.target.value)}
-            />
-          </label>
-          <br />
-
-          <label>
-            Test Duration (in minutes):
-            <input
-              type="number"
-              value={mockTestDuration}
-              onChange={(e) => setMockTestDuration(e.target.value)}
-            />
-          </label>
-          <br />
-
-          <label>
-            Question Type:
-            <select
-              value={selectedQuestionType}
-              onChange={(e) => setSelectedQuestionType(e.target.value)}
-            >
-              <option value="">Select Question Type</option>
-              <option value="singleCorrect">Single Correct</option>
-              <option value="multipleCorrect">Multiple Correct</option>
-              <option value="integer">Integer Type</option>
-              <option value="decimal">Decimal Type</option>
-            </select>
-          </label>
-          <br />
-
-          {selectedQuestionType && renderQuestionForm()}
-
-          <button onClick={createMockTest}>Create Mock Test</button>
-        </div>
-      )}
->>>>>>> cf212c931528e3fffafe32a42aee6f5958f00ba7
     </div>
   );
 };
